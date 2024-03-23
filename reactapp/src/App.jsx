e85c50dc8650a764364dae8fedf3f6ac619c440d
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-23 22:33:51 +0000

    datepicker npm package insatlled, age component created and rendered within app.jsx and date format set to be dd/mm/yyyyy

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
+import Age from './assets/components/ageform'
 
 import './App.css'
 
 function App() {
+  return(
+      <div>
+      <Age />
+  </div>
+  )
+
+
   
 }
 
---
+++ reactapp/src/assets/components/ageform.jsx
@@ -0,0 +1,21 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+  import React, { useState } from "react";
+
+
+const Age = () => {
+
+  const [date, setDate] = useState(new Date());
+  
+
+  return (
+    <>
+    <h2>Please input your child's Date of birth</h2>
+    <div>
+    <DatePicker  dateFormat="dd/MM/yyyy" selected={date} onChange={(date) => setDate(date)} />
+    </div>
+    </>
+    );
+}
+ 
+export default Age;
