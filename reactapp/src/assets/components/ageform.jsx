a53160645aef9170433e52d32313774f93934926
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-25 22:49:36 +0000

    commenting of code written in age component complete

--- reactapp/src/assets/components/ageform.jsx
+++ reactapp/src/assets/components/ageform.jsx
@@ -1,73 +1,79 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-  import React, { useEffect, useState } from "react";
-  import CustomInfo from "./custominfo";
-
+import React, { useEffect, useState } from "react";
+import CustomInfo from "./custominfo";
 
 const Age = () => {
 
+  // Initialise date to null and then set to date chosen by the user
+  // Initialised info page toggle based on user click
   const [date, setDate] = useState(null);
-  const [info, setInfo] = useState(false)
+  const [info, setInfo] = useState(false);
 
-  useEffect(()=>{
-    let onlyDate = date?.toISOString().split('T')[0];
-    localStorage.setItem("storedDate", JSON.stringify(onlyDate));
+  // store the birthdate input by user on the date picker
+  useEffect(() => {
+    let birthDate = date?.toISOString().split("T")[0];
+    localStorage.setItem("storedDate", JSON.stringify(birthDate));
 
-    console.log(onlyDate)
-  }, [date])
-  const displayinfo =()=>{
-    setInfo(true)
+    console.log(birthDate);
+  }, [date]);
+
+  // info page toggle done on clicking the button
+  const displayinfo = () => {
+    setInfo(true);
+  };
+
+  // conditional to check if info is truthy
+  if (info) {
+    return <CustomInfo />;
   }
-  
- 
-  
-  if(info){
-    return(
-      <CustomInfo />
-      )
-  }
 
-  const storeDate =() =>{
- 
-  
+  // retrieving stored birthdate from local storage
+  const storeDate = () => {
+    let retrievedDate = JSON.parse(localStorage.getItem("storedDate"));
 
-   
-   let retrievedDate = JSON.parse(localStorage.getItem("storedDate"))
-
-   if(!retrievedDate  ){
-    
-         console.log("Date is invalid");
-         return
- 
-   }
-   else{
-    console.log(retrievedDate)
-   }
-
-  
-}
+    if (!retrievedDate) {
+      console.log("Date is invalid");
+      return;
+    } else {
+      console.log(retrievedDate);
+    }
+  };
 
   return (
     <>
-    <h2>Please input your child's Date of birth</h2>
-    <div>
-    <DatePicker placeholderText="Click to select a date" dateFormat="dd/MM/yyyy"   
-      showMonthDropdown
-      showYearDropdown
-     
-     
-      dropdownMode="select" showIcon selected={date} onChange={(date) => { setDate(date)}}
-       />
-    </div>
-    {/* <div>
+      <h2>Please input your child's Date of birth</h2>
+      <div>
+        <DatePicker
+          placeholderText="Click to select a date"
+          dateFormat="dd/MM/yyyy"
+          showMonthDropdown
+          showYearDropdown
+          dropdownMode="select"
+          showIcon
+          selected={date}
+          onChange={(date) => {
+            setDate(date);
+          }}
+        />
+      </div>
+      {/* <div>
       Your child's age is : 
     </div> */}
-    <div>
-      <button onClick={()=> {{displayinfo}; storeDate(date)}}>Get Dental Information</button>
-    </div>
-   
+      <div>
+        <button
+          onClick={() => {
+            {
+              displayinfo;
+            }
+            storeDate(date);
+          }}
+        >
+          Get Dental Information
+        </button>
+      </div>
     </>
-    );
-}
- 
+  );
+};
+
 export default Age;
