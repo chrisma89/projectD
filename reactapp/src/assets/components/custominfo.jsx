9236d870395f3da799ae76f9618c2d2c7e8f7f07
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-04-09 22:11:46 +0100

    tooth componenet styling added to card body and card title

--- reactapp/src/assets/components/custominfo.jsx
+++ reactapp/src/assets/components/custominfo.jsx
@@ -64,7 +64,7 @@ const [childAge, setChildAge] = useState("")
   },[])
   
   return (
-    <div className="container-fluid d-flex flex-column align-items-center justify-content-center" style={{backgroundColor : "#032B43", height : "100vh", padding : "40px", color : "#FFBA08"}}>
+    <div className="container-fluid d-flex flex-column align-items-center justify-content-center" style={{backgroundColor : "#032B43", height : "", padding : "40px", color : "#FFBA08"}}>
       <h2> Dental Information</h2>
       <div>Your child is {childAge} {ageUnit} old</div>
       <ToothInfo childAge= {childAge} ageUnit ={ageUnit} />
--- reactapp/src/assets/components/tooth.jsx
+++ reactapp/src/assets/components/tooth.jsx
@@ -2,63 +2,52 @@ import dentalinfo from "../../dentalinfo.json";
 import CustomInfo from "./custominfo";
 import React from "react";
 import { useState } from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container"
+const ToothInfo = ({ childAge, ageUnit }) => {
+  let ageData = `${childAge} ${ageUnit}`;
+  let agedata = localStorage.getItem("agedata");
+  console.log(agedata);
 
-
-
-
-const ToothInfo = ({childAge, ageUnit}) => {
-
-  
-let ageData = `${childAge} ${ageUnit}`
-let agedata = localStorage.getItem("agedata")
-console.log(agedata)
-
-
-
-let matchedInfo = dentalinfo.find(info => agedata === info.id)
-
-
-
+  let matchedInfo = dentalinfo.find((info) => agedata === info.id);
 
   // if (childage)
-  return ( 
-    <div>  
+  return (
+    <div>
       {/* <div>{ageData}</div> */}
-    {matchedInfo ? (
-      <>  
-      
-    <Card>
-    <Card.Title style={{textAlign : "center"}}>Toothbrush :</Card.Title>
-      <Card.Body>{matchedInfo.toothbrush}</Card.Body>
-    </Card>
-    <Card>
-    <Card.Title>Brushing Frequency : </Card.Title>
-      <Card.Body>{matchedInfo.frequency}</Card.Body>
-    </Card>
-    <Card>
-    <Card.Title>Toothpaste : </Card.Title>
-      <Card.Body>{matchedInfo.toothpaste}</Card.Body>
-    </Card>
-    <Card>
-    <Card.Title>Visits to your dentist : </Card.Title>
-      <Card.Body>{matchedInfo.dentalvisit}</Card.Body>
-    </Card>
-    <Card>
-    <Card.Title>Dietary advice : </Card.Title>
-      <Card.Body>{matchedInfo.food}</Card.Body>
-    </Card>
-
-   
-      </>
-  )
-  :
-  (
-    <div>No matches found</div>
-  )}
-   
+      {matchedInfo ? (
+        <>
+          <Container className = "d-flex flex-column" style={{ textAlign: "center" }}>
+            {" "}
+            <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
+              <Card.Title style={{fontSize : "2rem"}}>
+                Toothbrush :
+              </Card.Title>
+              <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.toothbrush}</Card.Body>
+            </Card>
+            <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
+              <Card.Title style={{fontSize : "2rem"}}>Brushing Frequency : </Card.Title>
+              <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.frequency}</Card.Body>
+            </Card>
+            <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
+              <Card.Title style={{fontSize : "2rem"}}>Toothpaste : </Card.Title>
+              <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.toothpaste}</Card.Body>
+            </Card>
+            <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
+              <Card.Title style={{fontSize : "2rem"}}>Visits to your dentist : </Card.Title>
+              <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.dentalvisit}</Card.Body>
+            </Card>
+            <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
+              <Card.Title style={{fontSize : "2rem"}}>Dietary advice : </Card.Title>
+              <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.food}</Card.Body>
+            </Card>
+          </Container>
+        </>
+      ) : (
+        <div>No matches found</div>
+      )}
     </div>
-   );
-}
- 
+  );
+};
+
 export default ToothInfo;
