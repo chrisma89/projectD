6bfb399897c1a747fc8fd4631d59e818ee17352e
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-04-10 10:56:54 +0100

    icons added for food, schedule and attribution added and they are now appending on the page

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 - https://www.nationwidechildrens.org/family-resources-education/health-wellness-and-safety-resources/helping-hands/dental-teeth-and-gum-care-for-infants-and-toddlers
 <a href="https://www.flaticon.com/free-icons/toothbrush" title="toothbrush icons">Toothbrush icons created by Circlon Tech - Flaticon</a>
 <a href="https://www.flaticon.com/free-icons/clock" title="clock icons">Clock icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/healthcare-and-medical" title="healthcare and medical icons">Healthcare and medical icons created by HAJICON - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/calendar" title="calendar icons">Calendar icons created by SyafriStudio - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/healthy-food" title="healthy food icons">Healthy food icons created by tulpahn - Flaticon</a>
--- reactapp/src/assets/components/tooth.jsx
+++ reactapp/src/assets/components/tooth.jsx
@@ -26,19 +26,19 @@ const ToothInfo = ({ childAge, ageUnit }) => {
               <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.toothbrush}</Card.Body>
             </Card>
             <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
-              <Card.Title style={{fontSize : "2rem"}}>Brushing Frequency : </Card.Title>
+              <Card.Title style={{fontSize : "2rem"}}> <img src="public/icons/brushingfrquency.png" style={{height : "10vh", width : "auto"}}></img> </Card.Title>
               <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.frequency}</Card.Body>
             </Card>
             <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
-              <Card.Title style={{fontSize : "2rem"}}>Toothpaste : </Card.Title>
+              <Card.Title style={{fontSize : "2rem"}}> <img src="/icons/toothpaste.png" style={{height : "10vh", width : "auto"}}></img> </Card.Title>
               <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.toothpaste}</Card.Body>
             </Card>
             <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
-              <Card.Title style={{fontSize : "2rem"}}>Visits to your dentist : </Card.Title>
+              <Card.Title style={{fontSize : "2rem"}}><img src="/icons/calendar.png" style={{height : "10vh", width : "auto"}}></img> </Card.Title>
               <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.dentalvisit}</Card.Body>
             </Card>
             <Card style={{margin : "20px", padding : "20px", backgroundColor : "#FFBA08" , color : "#032B43"}}>
-              <Card.Title style={{fontSize : "2rem"}}>Dietary advice : </Card.Title>
+              <Card.Title style={{fontSize : "2rem"}}><img src="/icons/food.png" style={{height : "10vh", width : "auto"}}></img></Card.Title>
               <Card.Body style={{fontSize : "1.6rem"}}>{matchedInfo.food}</Card.Body>
             </Card>
           </Container>
